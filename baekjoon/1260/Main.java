5cce1b6c9ba719595c576fce50dfecbdec40ac79
yhames <wjddn494@naver.com>
2023-04-16 18:14:34 +0900

    1260 문제풀이 2번 완료

--- baekjoon/1260/Main.java
+++ baekjoon/1260/Main.java
@@ -1,61 +1,58 @@
-import java.util.*;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
 
 class Main {
-
+    static int node;
+    static int edge;
     static int[][] arr;
     static boolean[] isVisit;
     static Queue<Integer> queue = new LinkedList<>();
 
     public static void main(String[] args) {
-        int n;  // 정점의 개수
-        int m;  // 간선의 개수
-        int v;  // 시작 노드
-
         Scanner sc = new Scanner(System.in);
-        n = Integer.parseInt(sc.next());
-        m = Integer.parseInt(sc.next());
-        v = Integer.parseInt(sc.next());
+        node = Integer.parseInt(sc.next());
+        edge = Integer.parseInt(sc.next());
+        int root = Integer.parseInt(sc.next());
 
-        arr = new int[n + 1][n + 1];
-        isVisit = new boolean[n + 1];
-        for (int i = 0; i < m; i++) {
+        arr = new int[node + 1][node + 1];
+        isVisit = new boolean[node + 1];
+
+        for (int i = 0; i < edge; i++) {
             int x = Integer.parseInt(sc.next());
             int y = Integer.parseInt(sc.next());
             arr[x][y] = 1;
             arr[y][x] = 1;
         }
 
-        dfs(n, v);
+        dfs(root);
         System.out.println();
-        bfs(n, v);
+        bfs(root);
     }
 
-    static void dfs(int n, int v) {
-        if (isVisit[v]) {
-            return;
-        }
-
-        isVisit[v] = true;
-        System.out.print(v + " ");
-        for (int i = 1; i <= n; i++) {
-            if (arr[v][i] == 1) {
-                dfs(n, i);
+    static void dfs(int vertex) {
+        System.out.print(vertex + " ");
+        isVisit[vertex] = true;
+        for (int i = 1; i <= node; i++) {
+            if (arr[vertex][i] == 1 && !isVisit[i]) {
+                dfs(i);
             }
         }
     }
 
-    static void bfs(int n, int v) {
-        isVisit[v] = false;
-        queue.add(v);
+    static void bfs(int root) {
+        queue.add(root);
+        isVisit[root] = false;
         while (!queue.isEmpty()) {
-            int node = queue.poll();
-            System.out.print(node + " ");
-            for (int i = 1; i <= n; i++) {
-                if (arr[node][i] == 1 && isVisit[i]) {
+            int vertex = queue.poll();
+            System.out.print(vertex + " ");
+            for (int i = 1; i <= node; i++) {
+                if (arr[vertex][i] == 1 && isVisit[i]) {
                     queue.add(i);
                     isVisit[i] = false;
                 }
             }
         }
+
     }
 }

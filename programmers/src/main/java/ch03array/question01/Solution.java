3276306f7f709d4b6aad058bfae0a2cbe34d4024
yhames <wjddn494@naver.com>
2023-06-07 02:38:39 +0900

    reset

---
+++ programmers/src/main/java/ch03array/question01/Solution.java
@@ -0,0 +1,12 @@
+package ch03array.question01;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Solution {
+    public void solution() {
+        // TODO : ch03Array
+    }
+
+}
---
+++ programmers/src/test/java/ch03array/question01/SolutionTest.java
@@ -0,0 +1,20 @@
+package ch03array.question01;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+
+class SolutionTest {
+
+    @Test
+    @DisplayName("입력 예시1")
+    void test01() {
+
+    }
+
+    @Test
+    @DisplayName("입력 예시2")
+    void test02() {
+
+    }
+}

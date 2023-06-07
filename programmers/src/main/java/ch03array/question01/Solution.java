1a368f19c5f97af1436140bbfc2232881ec456fe
yhames <wjddn494@naver.com>
2023-06-08 00:21:11 +0900

    ch03 배열 -> 객체

--- programmers/src/main/java/ch03array/question01/Solution.java
+++ programmers/src/main/java/ch03array/question01/Solution.java
@@ -1,58 +1,33 @@
 package ch03array.question01;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class Solution {
     public void solution(List<int[]> line) {
-        List<int[]> intersectionPoints = new ArrayList<>();
         for (int i = 0; i < line.size(); i++) {
             for (int j = i + 1; j < line.size(); j++) {
-                intersectionPoints.add(getIntersectionPoint(line.get(i), line.get(j)));
+                Point intersectionPoints = getIntersectionPoints(line.get(i), line.get(j));
             }
         }
-
-        int[] widthAndHeight = getWidthAndHeight(intersectionPoints);
-        char[][] graph = new char[widthAndHeight[0]][widthAndHeight[1]];
-        for (char[] chars : graph) {
-            Arrays.fill(chars, '.');
-        }
-
+        
     }
 
-    private int[] getWidthAndHeight(List<int[]> vertexes) {
-        int[] maxVertex = getMaxVertex(vertexes);
-        int[] minVertex = getMinVertex(vertexes);
-        int width = maxVertex[0] - minVertex[0];
-        int height = maxVertex[1] - minVertex[1];
-        return new int[]{width, height};
-    }
+    public Point getIntersectionPoints(int[] intX, int[] intY) {
+        double[] x = intArrToDoubleArr(intX);
+        double[] y = intArrToDoubleArr(intY);
 
-    private int[] getMaxVertex(List<int[]> vertexes) {
-        int[] maxVertex = {0, 0};
-        for (int[] vertex : vertexes) {
-            if (vertex[0] > maxVertex[0]) {
-                maxVertex[0] = vertex[0];
-            }
-            if (vertex[1] > maxVertex[1]) {
-                maxVertex[1] = vertex[1];
-            }
+        if ((x[0] * y[1] - x[1] * y[0]) == 0) {
+            return null;
         }
-        return maxVertex;
-    }
+        double i = (x[1] * y[2] - x[2] * y[1]) / (x[0] * y[1] - x[1] * y[0]);
+        double j = (x[2] * y[1] - x[0] * y[2]) / (x[0] * y[1] - x[1] * y[0]);
+        double[] vertex = {i, j};
+        if (!isInteger(vertex)) {
+            return null;
+        }
+        return new Point(doubleArrToIntArr(vertex));
 
-    private int[] getMinVertex(List<int[]> vertexes) {
-        int[] minVertex = {0, 0};
-        for (int[] vertex : vertexes) {
-            if (vertex[0] < minVertex[0]) {
-                minVertex[0] = vertex[0];
-            }
-            if (vertex[1] < minVertex[1]) {
-                minVertex[1] = vertex[1];
-            }
-        }
-        return minVertex;
     }
 
     public boolean isInteger(double[] intersectionPoint) {
@@ -72,19 +47,17 @@ public class Solution {
         return Arrays.stream(x).mapToInt(d -> (int) d).toArray();
     }
 
-    public int[] getIntersectionPoint(int[] intX, int[] intY) {
-        double[] x = intArrToDoubleArr(intX);
-        double[] y = intArrToDoubleArr(intY);
+    public class Point {
 
-        if ((x[0] * y[1] - x[1] * y[0]) == 0) {
-            return null;
+        public final long x;
+
+        public final long y;
+
+        public Point(int[] line) {
+            this.x = line[0];
+            this.y = line[1];
         }
-        double i = (x[1] * y[2] - x[2] * y[1]) / (x[0] * y[1] - x[1] * y[0]);
-        double j = (x[2] * y[1] - x[0] * y[2]) / (x[0] * y[1] - x[1] * y[0]);
-        double[] vertex = {i, j};
-        if (isInteger(vertex)) {
-            return doubleArrToIntArr(vertex);
-        }
-        return null;
+
+
     }
 }
